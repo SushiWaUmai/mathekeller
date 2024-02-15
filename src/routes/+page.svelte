944385b02c0d7c003c720e6a09f603bc9aa02368
Eugene Matsumura <eugene.bb1@gmail.com>
2024-02-15 20:56:29 +0100

    feat: update home page with links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <div class="container mx-auto px-2 py-10">
-  <div class="w-full space-y-16 lg:w-2/3">
+	<div class="w-full space-y-16 lg:w-2/3">
 		<h1 class="h1">Willkommen zum Mathekeller</h1>
 	</div>
+
+	<ul>
+		<li>
+			<a href="/signum">Signum</a>
+		</li>
+		<li>
+			<a href="/addierwerk/von-neumann">Von Neumann</a>
+		</li>
+	</ul>
 </div>
