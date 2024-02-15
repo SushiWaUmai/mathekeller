63fa5faadcca835acd100909548445f5a4d22c6b
Eugene Matsumura <eugene.bb1@gmail.com>
2024-02-15 20:54:17 +0100

    feat: create von Neumann calculator

---
+++ src/lib/addierwerk/generateTable.ts
@@ -0,0 +1,46 @@
+import { tableMapperValues } from '@skeletonlabs/skeleton';
+import type { TableSource } from '@skeletonlabs/skeleton';
+
+const vonNeumann = (a: number, p: number) => {
+	const table: number[][] = [];
+
+	table.push([0, 0, 0]);
+	table.push([0, a, p]);
+
+	while (p !== 0) {
+		const aTemp = a ^ p;
+		const pTemp = (a & p) << 1;
+		const u = (pTemp & (1 << 4)) != 0 ? 1 : 0;
+		a = aTemp;
+		p = pTemp & 0b1111;
+
+		table.push([u, a, p]);
+	}
+
+	return table;
+};
+
+export const generateNeumannTable = (a: number, p: number) => {
+	const tableData = [];
+	const data = vonNeumann(a, p);
+
+	for (let i = 0; i < data.length; i++) {
+		const [u, a, p] = data[i];
+		tableData.push({
+			position: i + 1,
+			u: u,
+			accBin: a?.toString(2).padStart(4, '0'),
+			accDec: a + (u << 4),
+			pufBin: p?.toString(2).padStart(4, '0'),
+			pufDec: p,
+			s: p != 0 ? 1 : 0
+		});
+	}
+
+	const table: TableSource = {
+		head: ['U', 'Accumulator Binary', 'Accumulator', 'Puffer Binary', 'Puffer', 'S'],
+		body: tableMapperValues(tableData, ['u', 'accBin', 'accDec', 'pufBin', 'pufDec', 's'])
+	};
+
+	return table;
+};
---
+++ src/routes/addierwerk/von-neumann/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { generateNeumannTable } from '$lib/addierwerk/generateTable';
+	import { Table } from '@skeletonlabs/skeleton';
+
+	let acc: number = 0;
+	let puf: number = 0;
+
+	$: neumannTable = generateNeumannTable(acc, puf);
+</script>
+
+<div class="container mx-auto px-2 py-10 space-y-10">
+	<h1 class="h1">Von Neumann Addierwerk</h1>
+	<div class="flex space-x-3">
+		<label class="label">
+			<span>Accumulator</span>
+			<input type="number" class="input" bind:value={acc} />
+		</label>
+		<label class="label">
+			<span>Puffer</span>
+			<input type="number" class="input" bind:value={puf} />
+		</label>
+	</div>
+
+	<Table source={neumannTable} />
+</div>
