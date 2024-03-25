cb2aa3988d07355c737debe481effe4c6e5afbe1
Eugene Matsumura <eugene.bb1@gmail.com>
2024-03-25 18:01:59 +0100

    feat: update homepage with more links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,29 @@
-<div class="container mx-auto px-2 py-10">
+<div class="container mx-auto px-2 py-10 space-y-8">
 	<div class="w-full space-y-16 lg:w-2/3">
 		<h1 class="h1">Willkommen zum Mathekeller</h1>
 	</div>
 
-	<ul>
-		<li>
-			<a href="/signum">Signum</a>
-		</li>
-		<li>
-			<a href="/addierwerk/von-neumann">Von Neumann</a>
-		</li>
-	</ul>
+	<div>
+		<h2 class="h3">Diskrete Strukturen</h2>
+		<ul class="list-nav list-disc">
+			<li>
+				<a href="/signum">Signum</a>
+			</li>
+		</ul>
+	</div>
+
+	<div>
+		<h2 class="h3">Technische Informatik</h2>
+		<ul class="list-nav list-disc">
+			<li>
+				<a href="/addierwerk/parallel">Parallel Addierwerk</a>
+			</li>
+			<li>
+				<a href="/addierwerk/serial">Serien Addierwerk</a>
+			</li>
+			<li>
+				<a href="/addierwerk/von-neumann">Von Neumann Addierwerk</a>
+			</li>
+		</ul>
+	</div>
 </div>
